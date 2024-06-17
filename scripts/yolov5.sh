1472706bcac08ab355b7382f5fff60c20ae8f2bf
tobiasgmueller <tgmueller@ucdavis.edu>
2024-06-17 12:50:30 -0400

    added comments

--- scripts/yolov5.sh
+++ scripts/yolov5.sh
@@ -6,4 +6,6 @@ cp ./train.yaml ./yolov5/train.yaml
 cd ./yolov5
 pip install -r requirements.txt wandb
 
-python train.py --img=608 --epochs 2 --data train.yaml --weights yolov5s.pt
+# yolov5 defaults to batch=32. recommends starting with epoch=300
+# I think we should run at minimum yolov5m 
+python train.py --img=608 --epochs 300 --data train.yaml --weights yolov5m.pt
