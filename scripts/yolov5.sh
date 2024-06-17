cd2fd59b270a81aed43729003270a4dc46c23048
tobiasgmueller <tgmueller@ucdavis.edu>
2024-06-17 12:37:32 -0400

    added install wandb

--- scripts/yolov5.sh
+++ scripts/yolov5.sh
@@ -4,6 +4,6 @@ git clone https://github.com/ultralytics/yolov5
 cp ./train.yaml ./yolov5/train.yaml
 
 cd ./yolov5
-pip install -r requirements.txt
+pip install -r requirements.txt wandb
 
 python train.py --img=608 --epochs 2 --data train.yaml --weights yolov5s.pt
